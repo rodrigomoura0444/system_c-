7583d2a3e7be1f8d0e8451711bb95576ba87406a
Rodrigo Ferreira Moura <rodrigoferreiramoura@Mac.Home>
2024-10-21 00:56:31 +0100

    first change

---
+++ command.csproj/Program.cs
@@ -0,0 +1,26 @@
+﻿public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+{
+    if (env.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+    });
+}
